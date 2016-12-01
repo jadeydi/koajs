3b8f1375e350a433f9eabcbf5ca0dd297c0c0c5a
jadeydi <im.yuqlee@gmail.com>
2016-12-02 01:41:11 +0800

    Add user table

---
+++ migrations/20161201171945-create-users.js
@@ -0,0 +1,78 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      nickname: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      avatar_url: {
+        type: Sequelize.STRING,
+        defaultValue: ''
+      },
+      bio: {
+        type: Sequelize.TEXT
+      },
+      authentication_token: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      encrypted_password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      salt: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.dropTable('users');
+  }
+};
+
+/*
+CREATE DATABASE sand_dev;
+CREATE UNIQUE INDEX ON users ((LOWER(username)));
+ALTER TABLE users ADD CHECK (username ~* '^[a-z0-9][a-z0-9_]{3,31}$');
+CREATE UNIQUE INDEX ON users ((LOWER(email)));
+CREATE UNIQUE INDEX ON users (authentication_token);
+*/
