9f9d7a59f54475881334950195ee4b18d6c4fb99
jadeydi <im.yuqlee@gmail.com>
2016-12-28 19:49:58 +0800

    More test for model/user

--- test/models/user.js
+++ test/models/user.js
@@ -9,17 +9,35 @@ describe("models/user", function() {
 
   describe("#create with invalid attributes", function() {
     it('should not create user', function() {
-      this.user.create({password: "abc"}).then(function(user) {
+      this.user.create({}).then(function(user) {
+        console.info(user)
       }).catch(function(err) {
         assert.deepEqual(['username', 'email', 'password'].sort(), error.Extract(err));
       });
+      this.user.create({username: "!yuqlee", email: "yuqlee@gmail.com", password: "password"}).then(function(user) {
+        console.info(user)
+      }).catch(function(err) {
+        assert.deepEqual(['username'].sort(), error.Extract(err));
+      });
+      this.user.create({username: "yuqlee", email: "yuqleegmail.com", password: "password"}).then(function(user) {
+        console.info(user)
+      }).catch(function(err) {
+        assert.deepEqual(['email'].sort(), error.Extract(err));
+      });
+      this.user.create({username: "yuqlee", email: "yuqlee@gmail.com", password: "pass"}).then(function(user) {
+        console.info(user)
+      }).catch(function(err) {
+        assert.deepEqual(['password'].sort(), error.Extract(err));
+      });
     });
   });
+
   describe("#create", function() {
     it('should create valid user', function() {
-      return this.user.create({username: "yuqlee", email: "yuqlee@gmail.com", password: "password"}).then(function(user) {
+      this.user.create({username: "yuqlee", email: "yuqlee@gmail.com", password: "password"}).then(function(user) {
         assert.equal(user.username, "yuqlee")
         assert.equal(user.email, "yuqlee@gmail.com")
+        return user
       }).catch(function(err) {
         console.info(err)
       });
